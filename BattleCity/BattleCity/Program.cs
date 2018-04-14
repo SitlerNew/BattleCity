62fb2a1e1d3048eb05f696d3a6fb8eae63cb5281
SitlerNew <martyniuk.work@gmail.com>
2018-04-14 23:12:02 +0300

    testcommit

---
+++ BattleCity/BattleCity/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
+
+namespace BattleCity
+{
+    class Program
+    {
+        static RenderWindow window;
+
+        static void Main(string[] args)
+        {
+            window = new RenderWindow(new VideoMode(1280,800),"Battle City");
+            window.SetVerticalSyncEnabled(true);
+            window.Closed += WinClosed;
+
+            FieldMap map = new FieldMap("map.png");
+
+            Unit p = new Unit("players.png", 300,700,10);
+
+            Clock clock = new Clock();
+
+            float currentFrame = 0;
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                float time = clock.ElapsedTime.AsMicroseconds();
+                clock.Restart();
+                time = time / 800;
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+                {
+                    p.dir = 3;
+                    p.speed = 0.1f;
+                    currentFrame += 0.005f * time;
+
+                    if (currentFrame > 3)
+                        currentFrame -= 3;
+                    p.sprite.TextureRect = new IntRect(0, 0, 32, 32);
+                }
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+                {
+                    p.dir = 2;
+                    p.speed = 0.1f;
+                    currentFrame += 0.005f * time;
+
+                    if (currentFrame > 3)
+                        currentFrame -= 3;
+                    p.sprite.TextureRect = new IntRect(32,0, 32, 32);
+                }
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+                {
+                    p.dir = 0;
+                    p.speed = 0.1f;
+                    currentFrame += 0.005f * time;
+
+                    if (currentFrame > 3)
+                        currentFrame -= 3;
+
+                    p.sprite.TextureRect = new IntRect(96, 0, 32, 32);
+                }
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+                {
+                    p.dir = 1;
+                    p.speed = 0.1f;
+                    currentFrame += 0.005f * time;
+
+                    if (currentFrame > 3)
+                        currentFrame -= 3;
+
+                    p.sprite.TextureRect = new IntRect(64,0, 32, 32);
+                }
+
+                p.update(time,map.tileMap);
+
+                window.Clear(Color.Black);
+
+                map.Draw(ref window);
+                window.Draw(p.sprite);
+
+                window.Display();
+            }
+
+        }
+
+        public static void WinClosed(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+    }
+}
---
+++ BattleCity/BattleCity/Unit.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace BattleCity
+{
+    class Unit
+    {
+        private float x;
+        private float y;
+        public float dx;
+        public float dy;
+        public float width;
+        public float height;
+        public float speed;
+        public int dir;
+        private Image image;
+        private Texture texture;
+        public Sprite sprite;
+
+        public Unit(string name, float x, float y, float s = 10)
+        {
+            dx = 0;
+            dy = 0;
+            speed = s;
+            dir = 0;
+            width = 32;
+            height = 32;
+
+            image = new Image("..\\Source\\Textures\\" + name);
+            texture = new Texture(image);
+            sprite = new Sprite(texture);
+
+            this.x = x;
+            this.y = y;
+
+            sprite.TextureRect = new IntRect(0, 0, 32, 32);
+        }
+
+        public void update(float time, int[,] tileMap)
+        {
+            switch (dir)
+            {
+                case 0:
+                    dx = speed;
+                    dy = 0;
+                    break;
+                case 1:
+                    dx = -speed;
+                    dy = 0;
+                    break;
+                case 2:
+                    dx = 0;
+                    dy = speed;
+                    break;
+                case 3:
+                    dx = 0;
+                    dy = -speed;
+                    break;
+                default:
+                    break;
+            }
+
+            x += dx * time;
+            y += dy * time;
+
+            speed = 0;
+
+            sprite.Position = new Vector2f(x, y);
+            interactionWithMap(tileMap);
+        }
+
+        public void interactionWithMap(int[,] tileMap)
+        {
+            for (int i = (int)y / 32; i < (y + height) / 32; i++)
+                for (int j = (int)x / 32; j < (x + width) / 32; j++)
+                {
+                    if (tileMap[i, j] == 1)
+                    {
+                        if (dy > 0)
+                        {
+                            y = i * 32 - height;
+                        }
+                        if (dy < 0)
+                        {
+                            y = i * 32 + 32;
+                        }
+                        if (dx > 0)
+                        {
+                            x = j * 32 - width;
+                        }
+                        if (dx < 0)
+                        {
+                            x = j * 32 + 32;
+                        }
+                    }
+                }
+        }
+
+    }
+}
